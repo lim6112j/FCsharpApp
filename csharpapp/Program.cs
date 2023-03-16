1cb47ddf1b3aec26bb6a8dec248bc65df52876a1
ben lim <lim6112j@gmail.com>
2023-03-16 19:22:34 +0900

    initial commit

---
+++ csharpapp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FsharpApp;
+namespace CsharpApp;
+public class Program {
+  public static void Main(){
+    val type = FsharpApp.MyType.None;
+    Console.WriteLine("hello");
+  }
+}
---
+++ fsharpapp/Program.fs
@@ -0,0 +1,6 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+namespace FsharpApp
+module Program =
+  printfn "Hello from F#"
+module MyType =
+  type Option = S | None
