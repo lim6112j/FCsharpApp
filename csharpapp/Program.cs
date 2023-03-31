6402c0fe5d0cfe6915a49cb85930c39f20c96742
lim6112j <lim6112j@gmail.com>
2023-03-31 22:54:55 +0900

    print 2fold enumerable

--- csharpapp/Program.cs
+++ csharpapp/Program.cs
@@ -1,14 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using fsharpapp;
 namespace CsharpApp;
-public class Program {
-  public static void Main(){
-    // f# function
-    fsharpapp.Say.hello("lim");
-    // f# type -- type has no constructor , value needed from fsharp
-    var optionValue = MyTypes.value1;
-    Console.WriteLine(optionValue);
-    Console.WriteLine(String.Join(", ",Example.withYieldBang.ToList()));
-    Example.withYield.ToList().ForEach(Console.WriteLine);
-  }
+public class Program
+{
+    public static void Main()
+    {
+        // f# function
+        fsharpapp.Say.hello("lim");
+        // f# type -- type has no constructor , value needed from fsharp
+        var optionValue = MyTypes.value1;
+        Console.WriteLine(optionValue);
+        Console.WriteLine(String.Join(", ", Example.withYieldBang.ToList()));
+        Console.Write("[");
+        Example.withYield.ToList().ForEach(
+            x =>
+            {
+                Console.Write("[");
+                foreach (int i in x)
+                {
+                    if(i == x.Count() -1) {
+                    Console.Write(i);
+                    } else {
+                    Console.Write(i + " ,");
+                    }
+                }
+                Console.WriteLine("]");
+            }
+            );
+        Console.Write("]");
+    }
 }
